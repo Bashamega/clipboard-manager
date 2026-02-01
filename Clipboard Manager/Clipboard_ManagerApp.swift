0b2f71be9760a6ef53ef7aa04b9f400f3801a3f9
Bashamega <adambashaahmednaji@gmail.com>
2026-02-01 08:57:31 +0200

    Initial Commit

---
+++ Clipboard Manager/Clipboard_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clipboard_ManagerApp.swift
+//  Clipboard Manager
+//
+//  Created by adam Naji on 01/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Clipboard_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
